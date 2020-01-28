c50183282d045584e6c814bbc3aff3ed7d5f6c41
SergioCanto <enanifero@hotmail.com>
2020-01-27 19:40:26 -0600

    Actualizando

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.DATABASE_URL = process.env.DATABSE_URL || "mongodb://localhost/university";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/university";
 
 exports.PORT = process.env.PORT || 8080;
